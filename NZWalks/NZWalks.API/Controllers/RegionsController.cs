cd73ac45baf4017f0e7af5a723554cadebc51305
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-11-06 22:26:52 -0600

    starting DTO

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository regionRepository;
+
+        public RegionsController(IRegionRepository regionRepository)
+        {
+            this.regionRepository = regionRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllRegions()
+        {
+            var regions = regionRepository.GetAll();
+
+            // return DTO regions
+            var regionsDTO = new List<Models.DTO.Region>();
+            regions.ToList().ForEach(region =>
+            {
+                var regionDTO = new Models.DTO.Region()
+                {
+                    Id = region.Id,
+                    Code = region.Code,
+                    Name = region.Name,
+                    Area = region.Area,
+                    Lat = region.Lat,
+                    Long = region.Long,
+                    Population = region.Population
+                };
+            });
+
+            return Ok(regions);
+        }
+    }
+}
